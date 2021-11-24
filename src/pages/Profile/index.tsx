3a4f70142f8ab5f00532d6aa7b157267e1479974
NELO VICENTE DA SILVA NETO <201801203@unifoa.edu.br>
2021-11-23 22:02:51 -0300

    4avd_poo2_nelo_vicente

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { View } from "react-native";
+import {
+  Container,
+  Content,
+  Description,
+  Header,
+  List,
+  ListItem,
+  Title,
+  SubTitle,
+  ListItemText,
+} from "./styles";
+
+import server from "../../server.json";
+import { Avatar } from "../../components/Avatar";
+
+const Profile = ({ route }) => {
+  const [selectedEnv, setSelectedEnv] = useState("");
+
+  function handleSelectEnv(enviroKey) {
+    setSelectedEnv(enviroKey);
+  }
+
+  return (
+    <Container>
+      <Content>
+        <Header>
+          <View>
+            <Title>Olá,</Title>
+            <SubTitle>{route.params.name}</SubTitle>
+          </View>
+          <Avatar urlImage="https://avatars.githubusercontent.com/u/56004536?v=4"/>
+        </Header>
+        <Description>
+          Em qual ambiente{`\n`}você quer colocar sua planta?
+        </Description>
+        <List
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingRight: 40 }}
+        >
+          {server.plants_environments.map((enviro) => (
+            <ListItem
+              isSelected={selectedEnv === enviro.key}
+              onPress={() => handleSelectEnv(enviro.key)}
+              key={enviro.key}
+            >
+              <ListItemText isSelected={selectedEnv === enviro.key}>
+                {enviro.title}
+              </ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      </Content>
+    </Container>
+  );
+};
+
+export default Profile;
